389cafe8c2d340e2e4f9d41ffda5c30a9538df62
Henry Lin <henry.ys.lin@gmail.com>
2023-07-28 21:18:28 +0800

    Use full path specific to streamlit server

--- ecg-quiz.py
+++ ecg-quiz.py
@@ -21,7 +21,7 @@ sampling_rate = 100
 if not os.path.isfile(path + 'ptbxl_database.csv'):
     subprocess.run(['pip', 'uninstall', '-y', 'kaggle'])
     subprocess.run(['pip', 'install', '--user', 'kaggle'])
-    subprocess.run(['~/.local/bin/kaggle', 'datasets', 'download',
+    subprocess.run(['/home/appuser/.local/bin/kaggle', 'datasets', 'download',
                     'khyeh0719/ptb-xl-dataset', '--unzip'])
 
 # Configure libraries
